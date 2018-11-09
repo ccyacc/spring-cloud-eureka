ce49dd57428b979a8ecb42324ad975a1fd96206c
caichao@do1.com.cn <caichao@do1.com.cn>
2018-11-09 16:02:50 +0800

    提交

---
+++ spring-cloud-consumer/src/main/java/com/cc/controller/ConsumerController.java
@@ -0,0 +1,16 @@
+package com.cc.controller;
+
+import com.cc.remote.HelloRemote;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class ConsumerController {
+    @Autowired
+    private HelloRemote helloRemote;
+
+    @RequestMapping("/hello/{name}")
+    public String index(@PathVariable("name") String name) {
+        return helloRemote.hello(name);
+    }
+}
