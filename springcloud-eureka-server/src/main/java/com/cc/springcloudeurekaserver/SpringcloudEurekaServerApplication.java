decb2de3b19fd4ec5c228f52ae489a1a04aa2d9b
caichao@do1.com.cn <caichao@do1.com.cn>
2018-11-15 15:54:37 +0800

    提交

--- springcloud-eureka-server/src/main/java/com/cc/springcloudeurekaserver/SpringcloudEurekaServerApplication.java
+++ springcloud-eureka-server/src/main/java/com/cc/springcloudeurekaserver/SpringcloudEurekaServerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+//注册为客户端的注解
 @EnableDiscoveryClient
 public class SpringcloudEurekaServerApplication {
 
