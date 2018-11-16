dfb82616e207b03be36f16024d8b7531f8e45b90
caichao@do1.com.cn <caichao@do1.com.cn>
2018-11-16 18:05:02 +0800

    提交

--- spring-cloud-consumer/src/main/java/com/cc/springcloudconsumer/SpringCloudConsumerApplication.java
+++ spring-cloud-consumer/src/main/java/com/cc/springcloudconsumer/SpringCloudConsumerApplication.java
@@ -1,16 +1,36 @@
 package com.cc.springcloudconsumer;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+//启用Hystrix Dashboard功能
+@EnableHystrixDashboard
+//熔断超时断路器 默认熔断机制是1S
+@EnableCircuitBreaker
 public class SpringCloudConsumerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudConsumerApplication.class, args);
+	}
+
+	//springboot 版本如果是2.0则需要添加 ServletRegistrationBean 因为springboot的默认路径不是 "/hystrix.stream"，需要在自己的项目里配置servlet
+	@Bean
+	public ServletRegistrationBean getServlet(){
+		HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+		registrationBean.setLoadOnStartup(1);
+		registrationBean.addUrlMappings("/actuator/hystrix.stream");
+		registrationBean.setName("HystrixMetricsStreamServlet");
+		return registrationBean;
 	}
 }
